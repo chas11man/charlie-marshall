5abca1ac64577a4edc31668d96c2bc68e9fed334
Charlie Marshall <chas11man@gmail.com>
2014-03-17 02:52:50 -0400

    Home page and bootstrap added

--- charlie_marshall/settings.py
+++ charlie_marshall/settings.py
@@ -46,6 +46,10 @@ INSTALLED_APPS = (
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
+    # 3rd party
+    'django_extensions',
+    'bootstrap3',
+    # apps
     'polls',
 )
 
--- charlie_marshall/urls.py
+++ charlie_marshall/urls.py
@@ -1,12 +1,10 @@
 from django.conf.urls import patterns, include, url
-
 from django.contrib import admin
+from polls.views import home
 admin.autodiscover()
 
 urlpatterns = patterns('',
     # Examples:
-    # url(r'^$', 'charlie_marshall.views.home', name='home'),
-    # url(r'^blog/', include('blog.urls')),
-
+    url(r'^$', home, name='home'),
     url(r'^admin/', include(admin.site.urls)),
 )
