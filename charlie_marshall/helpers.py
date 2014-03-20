7ea62e15aa0f7ad4e2a5dac5e01f788ab0a851bd
Charlie Marshall <chas11man@gmail.com>
2014-03-20 14:05:06 -0400

    Fixed certificate errors

--- charlie_marshall/helpers.py
+++ charlie_marshall/helpers.py
@@ -1,5 +1,16 @@
 from storages.backends.s3boto import S3BotoStorage
 from django.conf import settings
 
-StaticRootS3BotoStorage = lambda: S3BotoStorage(location='static', bucket=settings.AWS_BUCKET_NAME)
-MediaRootS3BotoStorage  = lambda: S3BotoStorage(location='media', bucket=settings.AWS_BUCKET_NAME)
+class FixedS3BotoStorage(S3BotoStorage):
+	def __init__(self, *args, **kwargs):
+		super(FixedS3BotoStorage, self).__init__(bucket=settings.AWS_BUCKET_NAME, *args, **kwargs)
+
+	def url(self, name):
+		url = super(FixedS3BotoStorage, self).url(name)
+		bucket_name = self.bucket.name
+		if "%s.s3.amazonaws.com" % bucket_name in url:
+			url = url.replace("%s.s3.amazonaws.com/" % bucket_name, "s3.amazonaws.com/%s/" % bucket_name)
+		return url
+
+StaticRootS3BotoStorage = lambda: FixedS3BotoStorage(location='static')
+MediaRootS3BotoStorage  = lambda: FixedS3BotoStorage(location='media')
