e69d730d71b04603102bdab4e649c13fbd21b224
Charlie Marshall <chas11man@gmail.com>
2014-03-20 01:44:56 -0400

    image

--- polls/views.py
+++ polls/views.py
@@ -2,4 +2,4 @@ from django.shortcuts import render
 from photologue.models import Photo
 
 def home(request):
-    return render(request, 'index.html', {'photo': Photo.objects.all()[0]})
+    return render(request, 'index.html', {'photo': Photo.objects.all()[0].image.url})
