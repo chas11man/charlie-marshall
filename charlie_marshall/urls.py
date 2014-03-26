9b1b6334ebe9617631a8808488906295a4e9ca70
Charlie Marshall <chas11man@gmail.com>
2014-03-26 18:11:12 -0400

    Many more views

--- charlie_marshall/urls.py
+++ charlie_marshall/urls.py
@@ -1,11 +1,13 @@
 from django.conf.urls import patterns, include, url
 from django.contrib import admin
-from polls.views import home
+from polls.views import blog, blog_paged, blog_month, blog_month_paged
 admin.autodiscover()
 
 urlpatterns = patterns('',
-	url(r'^$', home, name='home'),
-	url(r'^page(?P<page>\d+)/$', home, name='paged_home'),
+	url(r'^$', blog, name='blog'),
+	url(r'^page/(?P<page>\d+)/$', blog_paged, name='blog_paged'),
+	url(r'^month/(?P<month>\d{4})/$', blog_month, name='blog_month'),
+	url(r'^month/(?P<month>\d{4})/page/(?P<page>\d+)/$', blog_month_paged, name='blog_month_paged'),
 	url(r'^admin/', include(admin.site.urls)),
 	url(r'^photologue/', include('photologue.urls')),
 	url(r'^tinymce/', include('tinymce.urls')),
--- polls/views.py
+++ polls/views.py
@@ -1,21 +1,59 @@
-from django.shortcuts import render
+from django.http import Http404
+from django.shortcuts import render, redirect
 from polls.models import Blog_Post
 
-def home(request, page=1):
-	page = int(page)
-	if page < 1:
-		page = 1
-	end_post = (page * 5)
-	posts = Blog_Post.objects.all().order_by('-posted')[end_post-5:end_post]
+def blog_helper(request, posts, page=1):
 	if len(posts) == 0:
-		page -= 1;
-		return home(request, page)
-	elif len(posts) < 5:
-		end = True
+		if page == 1:
+			raise Http404
+		else:
+			page -= 1;
+			return redirect('blog_paged', page=page)
+	elif len(posts) < 6:
+		next_page = False
 	else:
-		end = False
-	months = []
+		next_page = True
+		posts = posts[:5]
+
+	months = {}
 	for post in Blog_Post.objects.all().order_by('-posted'):
 		if post.posted.strftime('%B \'%y') not in months:
-			months.append(post.posted.strftime('%B \'%y'))
-	return render(request, 'index.html', {'posts': posts, 'page': page, 'end': end, 'months': months, })
+			months[post.posted.strftime('%m%y')] = post.posted.strftime('%B \'%y')
+
+	context = {'posts': posts, 'page': page, 'next_page': next_page, 'months': months, }
+	return render(request, 'index.html', context)
+
+
+def blog(request):
+	posts = Blog_Post.objects.all().order_by('-posted')[:6]
+
+	return blog_helper(request, posts)
+
+def blog_paged(request, page):
+	page = int(page)
+	if page <= 1:
+		return redirect('blog')
+
+	end_post = (page - 1) * 5
+	posts = Blog_Post.objects.all().order_by('-posted')[end_post:end_post+6]
+
+	return blog_helper(request, posts, page)
+
+def blog_month(request, month):
+	mon = month[:2]
+	year = month[2:]
+	posts = Blog_Post.objects.filter(posted__year='20{0}'.format(year), posted__month='{0}'.format(mon)).order_by('-posted')[:6]
+
+	return blog_helper(request, posts)
+
+def blog_month_paged(request, month, page):
+	page = int(page)
+	if page <= 1:
+		return redirect('blog_month', month=month)
+
+	end_post = (page - 1) * 5
+	mon = month[:2]
+	year = month[2:]
+	posts = Blog_Post.objects.filter(posted__year='20{0}'.format(year), posted__month='{0}'.format(mon)).order_by('-posted')[end_post:end_post+6]
+
+	return blog_helper(request, posts, page)
